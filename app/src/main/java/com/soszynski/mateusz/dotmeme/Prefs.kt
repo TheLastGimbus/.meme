92a9ad9d26ed42012d11b33547b6162943634cb8
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2019-12-03 00:17:55 +0100

    Added rest of the stuff for preference

--- app/src/main/java/com/soszynski/mateusz/dotmeme/Prefs.kt
+++ app/src/main/java/com/soszynski/mateusz/dotmeme/Prefs.kt
@@ -8,12 +8,14 @@ class Prefs {
         companion object {
             const val SHOW_NEW_FOLDER_NOTIFICATION = "pref_show_new_folder_notification"
             const val FIRST_LAUNCH = "pref_first_app_launch"
+            const val ADD_WATERMARK = "add_watermark_on_share"
         }
     }
 
     class Defaults {
         companion object {
             const val SHOW_NEW_FOLDER_NOTIFICATION = true
+            const val ADD_WATERMARK = false
         }
     }
 }
